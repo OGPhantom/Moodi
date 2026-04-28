9450fce6dfb23ec1fdb46380e28f198a84472877
OGPhantom <nikita.storchai@nure.ua>
2026-04-28 13:19:24 +0300

    Initial commit

---
+++ Moodi/MoodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodiApp.swift
+//  Moodi
+//
+//  Created by Никита Сторчай on 28.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct MoodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
